dacad6876e876355940abde47a2035c5ead4bde7
Glitch (abounding-reaction) <none>
2017-12-05 20:07:06 +0000

    clean up non-relevant commented out files

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // init project
 var express = require('express');
-var app = express();
 var requestIp = require('request-ip');
+var app = express();
+
 var obj = {
   "ipaddress": "null",
   "language": "null",
@@ -60,60 +61,3 @@ app.get('/:api/whoami', function(req, res, next) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-
-
-     /*if (key === "x-forwarded-for" || key === "user-agent" || key === "accept-language") {  
-      obj.ipaddress = agent[key];
-
-    }*/
-
-
-/*
-
-app.use(requestIp.mw())
-
-app.use(function(req, res, next) {      
-       obj.ipaddress = req.clientIp;
-       next();
-   });
-
-app.get('/:api/whoami', function(req, res, next) {
-     var lan = req.acceptsLanguages();
-     var enc = req.headers;
-     obj.language = lan[0];
-     obj.software = enc;
-     res.json(obj);
-});
-
-*/
-
-
-
-
-
-/*
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-*/
